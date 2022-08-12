8573d81a16fda388ebb0408e5b76a755fe4f194b
Joshua-Micheletti <joshua.e.n.micheletti@gmail.com>
2022-08-12 20:00:56 +0200

    added starting commands

--- index.js
+++ index.js
@@ -23,6 +23,81 @@ app.post('/restart', (req, resp) => {
     resp.status(200).end();
 });
 
+app.post('/start', (req, resp) => {
+    exec('sudo systemctl start minecraft', (error, stdout, stderr) => {
+        if (error) {
+            console.error('error: ' + error.message);
+            return;
+        }
+
+        if (stderr) {
+            console.error('stderr: ' + stderr);
+            return;
+        }
+
+        console.log('stdout:\n' + stdout);
+    });
+
+    resp.status(200).end();
+});
+
+app.post('/stop', (req, resp) => {
+    exec('sudo systemctl stop minecraft', (error, stdout, stderr) => {
+        if (error) {
+            console.error('error: ' + error.message);
+            return;
+        }
+
+        if (stderr) {
+            console.error('stderr: ' + stderr);
+            return;
+        }
+
+        console.log('stdout:\n' + stdout);
+    });
+
+    resp.status(200).end();
+});
+
+app.get('/checkStatus', (req, resp) => {
+    exec('sudo systemctl status minecraft', (error, stdout, stderr) => {
+        if (error) {
+            console.error('error: ' + error.message);
+            return;
+        }
+
+        if (stderr) {
+            console.error('stderr: ' + stderr);
+            return;
+        }
+
+        console.log('stdout:\n' + stdout);
+    });
+
+    resp.status(200).end();
+});
+
+app.post('/sendCommand', (req, resp) => {
+    var command = req.query.command;
+
+    exec("echo " + command + " > /run/minecraft.stdin", (error, stdout, stderr) => {
+        if (error) {
+            console.error('error: ' + error.message);
+            return;
+        }
+
+        if (stderr) {
+            console.error('stderr: ' + stderr);
+            return;
+        }
+
+        console.log('stdout:\n' + stdout);
+    });
+
+    resp.status(200).end();
+});
+
+
 const listener = app.listen(3000, () => {
     console.log("Your app is listening on port " + listener.address().port);
 });
